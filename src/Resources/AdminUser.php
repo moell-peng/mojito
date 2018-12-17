00de918ab5deaf11bdb20aefb6fdfe49ceb372b3
osi <yaoiluo@gmail.com>
2018-12-17 23:16:56 +0800

    [change]AdminUserResource,roles,avatar,mobile

--- src/Resources/AdminUser.php
+++ src/Resources/AdminUser.php
@@ -2,19 +2,19 @@
 
 namespace Moell\Mojito\Resources;
 
-
 use Illuminate\Http\Resources\Json\Resource;
 
-class AdminUser extends Resource
-{
-    public function toArray($request)
-    {
-        return [
-            'id'    => $this->id,
-            'name'  => $this->name,
-            'email' => $this->email,
-            'created_at' => (string)$this->created_at,
-            'updated_at' => (string)$this->updated_at
-        ];
-    }
+class AdminUser extends Resource {
+	public function toArray($request) {
+		return [
+			'id' => $this->id,
+			'name' => $this->name,
+			'email' => $this->email,
+			'avatar' => $this->avatar,
+			'mobile' => $this->mobile,
+			'roles' => $this->roles->pluck('name'),
+			'created_at' => (string) $this->created_at,
+			'updated_at' => (string) $this->updated_at,
+		];
+	}
 }

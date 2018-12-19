0fd9aa14c4127840aeaf4ce443b462ba23a87d43
moell <moell91@foxmail.com>
2018-12-19 23:09:27 +0800

    Update routers.js

--- resources/router/routers.js
+++ resources/router/routers.js
@@ -37,7 +37,7 @@ export default [
       {
         path: 'role',
         meta: {
-		  provider: 'admin',
+          provider: 'admin',
           title: 'role',
           cache: true,
         },
